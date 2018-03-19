75d6398e5775e50530ee6c545e36527d0fb59dcc
Joshua Koo <zz85nus@gmail.com>
2018-03-19 10:22:03 +0800

    Debugging webgl2

--- webgl2.js
+++ webgl2.js
@@ -1,8 +1,13 @@
-var gl;
+var gl, program, vao;
 
 function init() {
     // follow this! https://webgl2fundamentals.org/webgl/lessons/webgl-fundamentals.html
     var canvas = document.createElement('canvas');
+    // canvas.width = innerWidth;
+    // canvas.height = innerHeight;
+    canvas.width = 400;
+    canvas.height = 300;
+
     document.body.appendChild(canvas);
     gl = canvas.getContext('webgl2');
 
@@ -43,6 +48,67 @@ function init() {
 
     var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
     var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    program = createProgram(gl, vertexShader, fragmentShader);
+    
+    // attributes
+    var positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+
+    // buffers to power attributes
+    var positionBuffer = gl.createBuffer();
+    
+    // bind buffer to attribute
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // fill buffers
+    var positions = [
+        0, 0,
+        0, 0.5,
+        0.7, 0,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    // Vertex Array Object (attribute state)
+    var vao = gl.createVertexArray();
+    
+    gl.bindVertexArray(vao);
+    
+    // we want data out of our buffers
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    var size = 2; // 2 components per iterations
+    var type = gl.FLOAT; // 32 bits
+    var normalize = false; // dont normalize data
+    var stride = 0; // size * sizeof(type) to get to next
+    var offset = 0; // 
+
+    gl.vertexAttribPointer(
+        positionAttributeLocation, size, type, normalize, stride, offset
+    );
+    
+
+    drawScene();
+}
+
+function drawScene() {
+    resize(gl.canvas); // resize canvas
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height); // set viewport (remap -1..1)
+    
+    // clear canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // tell webgl which program to use
+    gl.useProgram(program);
+
+    // bind the attributes
+    gl.bindVertexArray(vao);
+
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 3;
+
+    gl.drawArrays(primitiveType, offset, count);
 }
 
 function createShader(gl, type, source) {
@@ -58,10 +124,43 @@ function createShader(gl, type, source) {
     var log = gl.getShaderInfoLog(shader);
     gl.deleteShader(shader);
 
-    console.log('Failed to compile\n')
+    console.log('Failed to compile')
     console.log(log);
     var code = source.split('\n').map((line, no) => `${no + 1}: ${line}`).join('\n');
     console.log(code);
 }
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    var log = gl.getProgramInfoLog(program);
+    console.log('Error creating program');
+    console.log(log);
+
+    gl.deleteProgram(program);
+}
+
+function resize(canvas) {
+    // Lookup the size the browser is displaying the canvas.
+    var displayWidth  = canvas.clientWidth;
+    var displayHeight = canvas.clientHeight;
+   
+    // Check if the canvas is not the same size.
+    if (canvas.width  !== displayWidth ||
+        canvas.height !== displayHeight) {
+   
+      // Make the canvas the same size
+      canvas.width  = displayWidth;
+      canvas.height = displayHeight;
+    }
+  }
 
 init();
